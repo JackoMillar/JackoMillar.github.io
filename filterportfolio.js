ba4fa6ecb64c386a494a404c753c8458c9c4919b
Jack Millar <99406899+JackoMillar@users.noreply.github.com>
2024-07-10 21:02:17 +0100

    new project added

--- filterportfolio.js
+++ filterportfolio.js
@@ -38,59 +38,60 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
+function toggleContent(button) {
+    var textContent = button.parentElement;
+    textContent.classList.toggle("expanded");
+    if (textContent.classList.contains("expanded")) {
+        button.textContent = "Read less";
+    } else {
+        button.textContent = "Read more";
+    }
+}
 
-        function toggleContent(button) {
-            var textContent = button.parentElement;
-            textContent.classList.toggle("expanded");
-            if (textContent.classList.contains("expanded")) {
-                button.textContent = "Read less";
+function filterSelection(c) {
+    var x, i, projects, tags, tagElements, j, t;
+    projects = document.getElementsByClassName("project-section");
+    if (c == "all") { t = true; } else { t = false; }
+    console.log("Filtering projects by tag: " + c); // Debugging line
+    for (i = 0; i < projects.length; i++) {
+        w3RemoveClass(projects[i], "show");
+        tags = projects[i].getElementsByClassName("tag");
+        for (j = 0; j < tags.length; j++) {
+            if (t == false) {
+                console.log("Checking project tag: " + tags[j].className); // Debugging line
+                var regex = new RegExp("\\b" + c + "\\b", "i");
+                if (regex.test(tags[j].className)) {
+                    w3AddClass(projects[i], "show");
+                    break;
+                }
             } else {
-                button.textContent = "Read more";
+                w3AddClass(projects[i], "show");
+                break;
             }
         }
+    }
+}
 
-        function filterSelection(c) {
-            var x, i, projects, tags, tagElements, j, t;
-            projects = document.getElementsByClassName("project-section");
-            if (c == "all") { t = true; } else { t = false; }
-            for (i = 0; i < projects.length; i++) {
-                w3RemoveClass(projects[i], "show");
-                tags = projects[i].getElementsByClassName("tag");
-                for (j = 0; j < tags.length; j++) {
-                    if (t == false) {
-                        var regex = new RegExp("\\b" + c + "\\b", "i");
-                        if (regex.test(tags[j].className)) {
-                            w3AddClass(projects[i], "show");
-                            break;
-                        }
-                    } else {
-                        w3AddClass(projects[i], "show");
-                        break;
-                    }
-                }
-            }
+function w3AddClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {
+            element.className += " " + arr2[i];
         }
-        
-        function w3AddClass(element, name) {
-            var i, arr1, arr2;
-            arr1 = element.className.split(" ");
-            arr2 = name.split(" ");
-            for (i = 0; i < arr2.length; i++) {
-                if (arr1.indexOf(arr2[i]) == -1) {
-                    element.className += " " + arr2[i];
-                }
-            }
+    }
+}
+
+function w3RemoveClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
         }
-        
-        function w3RemoveClass(element, name) {
-            var i, arr1, arr2;
-            arr1 = element.className.split(" ");
-            arr2 = name.split(" ");
-            for (i = 0; i < arr2.length; i++) {
-                while (arr1.indexOf(arr2[i]) > -1) {
-                    arr1.splice(arr1.indexOf(arr2[i]), 1);
-                }
-            }
-            element.className = arr1.join(" ");
-        }
-        
+    }
+    element.className = arr1.join(" ");
+}
+
